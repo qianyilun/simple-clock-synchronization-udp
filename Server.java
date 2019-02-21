c8d61fea4a7bc51a3c7287465d2ba6c17557b24c
Allen Qian <qianyilun@gmail.com>
2019-02-21 15:38:28 -0800

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,44 +1,54 @@
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.sql.Date;
+import java.util.*;
 
 public class Server {
-    public static void main(String[] args) {
-        DatagramSocket serverSocket = new DatagramSocket(1234); // at port 1234
-        byte[] recevied = new byte[65535]; // magic number
-        byte[] send = new byte[65535];
+    public static void main(String[] args){
+        DatagramSocket serverSocket = null;
+        try {
+            serverSocket = new DatagramSocket(1234); // at port 1234
+            InetAddress inetAddress = InetAddress.getLocalHost();
+            System.out.println("Server IP: " + inetAddress.getHostAddress());
+            System.out.println("Server port: 1234");
+            System.out.println("Server is listening...");
 
-        DatagramPacket dpReceive = null;
+            
+            byte[] recevied = new byte[1024]; // magic number
+            byte[] send = new byte[1024];
 
-        while (true) {
-            dpReceive = new DatagramPacket(recevied, recevied.length);
-            ds.receive(dpReceive);
+            DatagramPacket dpReceive = null;
 
-            System.out.println("Server recevied!");
+            while (true) {
+                dpReceive = new DatagramPacket(recevied, recevied.length);
+                serverSocket.receive(dpReceive);
 
-            InetAddress ip = dpReceive.getAddress();
-            int port = dpReceive.getPort();
+                System.out.println("Server recevied!");
 
-            send = getServerTime().getBytes();
+                InetAddress ip = dpReceive.getAddress();
+                int port = dpReceive.getPort();
 
-            DatagramPacket sendPacket = new DatagramPacket(send, send.length, ip, port);
-            serverSocket.send(sendPacket);
+                String serverTime = getServerTime();
+
+                System.out.println(serverTime);
+
+                send = serverTime.getBytes();
+
+                DatagramPacket sendPacket = new DatagramPacket(send, send.length, ip, port);
+                serverSocket.send(sendPacket);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            serverSocket.close();
         }
     }
 
     private static String getServerTime() {
         Calendar cal = Calendar.getInstance();
         Date date = cal.getTime();
-
         long timeInMillis = date.getTime(); 
-        int hour = cal.get(Calendar.HOUR);
-        int min = cal.get(Calendar.MINUTE);
-        int sec = cal.get(Calendar.SECOND);
-        int msec = cal.get(Calendar.MILLISECOND);
-
-        System.out.println("Get sever time: " + hour + ":" + min + ":" + sec + ":" + msec);
-        
+        System.out.println(date);
         return String.valueOf(timeInMillis);
     }
 }
