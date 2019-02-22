7179bdb30bb41ff658067ec1e3419e5de249a4db
Allen Qian <qianyilun@gmail.com>
2019-02-21 16:26:19 -0800

    add more comments

--- Client.java
+++ Client.java
@@ -8,23 +8,30 @@ public class Client {
         // start the "timer"
         long startTimeInMillis = System.currentTimeMillis();
 
+        // create a client datagramSocket instance
         DatagramSocket clientSocket = new DatagramSocket();
 
+        // buffs to store data
         byte[] sendData = new byte[1024];
         byte[] receiveData = new byte[1024];
 
+        // get server's ip instance
         InetAddress serverIp = InetAddress.getByName("192.168.0.15");
+
+        // send request by udp
         DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverIp, 1234);
         clientSocket.send(sendPacket);
 
+        // receive response from server by udp
         DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
         clientSocket.receive(receivePacket);
 
         String serverResponse = new String(receivePacket.getData());
-        System.out.println(serverResponse);
+        System.out.println("Server's response: " + serverResponse);
+
         long serverTimeInMillis = Long.parseLong(serverResponse.trim()); 
         
-        // stop the "timer"
+        // stop the "timer" and caculate RTT
         long endTimeInMillis = System.currentTimeMillis();
         long calculatedClientTime = (endTimeInMillis - startTimeInMillis) / 2 + serverTimeInMillis;
         Calendar newClientTime = Calendar.getInstance();
@@ -33,6 +40,7 @@ public class Client {
         System.out.println("calculated Client Time in Milliseconds is: " + calculatedClientTime);
         System.out.println("calculated Client Time is: " + newClientTime.getTime());
 
+        // close connection
         clientSocket.close();
     }
 }
--- Server.java
+++ Server.java
@@ -7,19 +7,22 @@ public class Server {
     public static void main(String[] args){
         DatagramSocket serverSocket = null;
         try {
+            // create a instance of server datagramSocket
             serverSocket = new DatagramSocket(1234); // at port 1234
             InetAddress inetAddress = InetAddress.getLocalHost();
             System.out.println("Server IP: " + inetAddress.getHostAddress());
             System.out.println("Server port: 1234");
             System.out.println("Server is listening...");
 
-            
+            // buf to store data
             byte[] recevied = new byte[1024]; // magic number
             byte[] send = new byte[1024];
 
             DatagramPacket dpReceive = null;
 
+            // keep listening
             while (true) {
+                // receive packet once request is in
                 dpReceive = new DatagramPacket(recevied, recevied.length);
                 serverSocket.receive(dpReceive);
 
@@ -28,12 +31,12 @@ public class Server {
                 InetAddress ip = dpReceive.getAddress();
                 int port = dpReceive.getPort();
 
+                // get server's time
                 String serverTime = getServerTime();
-
                 System.out.println(serverTime);
 
+                // send server's time back to client by udp
                 send = serverTime.getBytes();
-
                 DatagramPacket sendPacket = new DatagramPacket(send, send.length, ip, port);
                 serverSocket.send(sendPacket);
             }
